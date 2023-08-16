4f3020b21d07ac3bdb90c10d3852df286dde9080
kaveenhyacinth <alwiskaveen@gmail.com>
2023-08-16 21:35:44 +0530

    setup sidebar

---
+++ src/views/EmailArchive.vue
@@ -0,0 +1,7 @@
+<script>
+export default {}
+</script>
+
+<template>Archive</template>
+
+<style scoped></style>
---
+++ src/views/EmailInbox.vue
@@ -0,0 +1,7 @@
+<script>
+export default {}
+</script>
+
+<template>Inbox</template>
+
+<style scoped></style>
